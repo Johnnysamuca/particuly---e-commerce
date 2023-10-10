a261cc9b322d5e17e040a1c00745e02f26dd82f3
Samuel <johnnysamuel486@gmail.com>
2023-10-10 14:55:05 -0300

    interação de aba

---
+++ loja/loja.js
@@ -0,0 +1,11 @@
+
+function normal(){
+    let aba = document.getElementById('aba')
+    aba.style.visibility = "hidden"
+    
+}
+
+function aba(){
+    let aba = document.getElementById('aba')
+    aba.style.visibility = ""
+} 
